aa563d3565b5ac61152d233673b4e8bfdb63dd3b
Rachel Strasdin <79751917+strasdin@users.noreply.github.com>
2021-02-27 08:37:03 -0600

    d1 and d2 done

--- 2015/2a.js
+++ 2015/2a.js
@@ -12,5 +12,31 @@ try {
 let input = [];
 
 data.split('\r\n').forEach(element => {
-    input.push(element);
+    let temp = element;
+    input.push(temp);
+    
 });
+
+let total = 0;
+
+input.forEach(element => {
+    let nums = element.split('x');
+
+    let l = nums[0];
+    let w = nums[1];
+    let h = nums[2];
+
+    let s1 = l * w * 2;
+    let s2 = l * h * 2;
+    let s3 = h * w * 2;
+
+    let arr = [s1, s2, s3].sort(function(a, b) {
+        return a - b;
+    });
+    
+    let slack = arr[0] / 2;
+    
+    total +=  s1 + s2  + s3 + slack;
+});
+
+log(total);
--- 2015/2b.js
+++ 2015/2b.js
@@ -12,5 +12,27 @@ try {
 let input = [];
 
 data.split('\r\n').forEach(element => {
-    input.push(element);
+    let temp = element;
+    input.push(temp);
+    
 });
+
+let total = 0;
+
+input.forEach(element => {
+    let nums = element.split('x');
+    
+    let l = nums[0];
+    let w = nums[1];
+    let h = nums[2];
+
+    let arr = [l, w, h].sort(function(a, b) {
+        return a - b;
+    });
+    
+    let ribbon = arr[0] * 2 + arr[1] * 2;
+    let bow = l * w * h;
+    total += ribbon + bow;
+});
+
+log(total);
