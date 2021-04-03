650a6736c4381af1e130d4c551148df1ec6c91e7
Rachel Strasdin <79751917+strasdin@users.noreply.github.com>
2021-04-03 08:34:26 -0500

    Add More Days

---
+++ 2015/10b.js
@@ -0,0 +1,24 @@
+const fs = require('fs')
+const LINQ = require('node-linq').LINQ;
+const {log} = require('../common/log');
+
+let data = '';
+try {
+    data = fs.readFileSync('2015/inputs/10.txt', 'utf8');
+} catch (err) {
+    console.error(err);
+}
+
+let input = [];
+
+data.split('\r\n').forEach(element => {
+    let temp = element;
+    input.push(temp);
+    
+});
+
+let total = 0;
+
+
+
+log(total);
