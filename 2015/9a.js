63270319fe8cfd8b6465452281e7c2a1874bed03
Rachel Strasdin <79751917+strasdin@users.noreply.github.com>
2021-04-10 08:59:50 -0500

    Day 9

--- 2015/9a.js
+++ 2015/9a.js
@@ -12,13 +12,59 @@ try {
 let input = [];
 
 data.split('\r\n').forEach(element => {
-    let temp = element;
+    let arr = element.split(' ');
+    let temp = {
+        loc1: arr[0],
+        loc2: arr[2],
+        distance: parseInt(arr[4])
+    }
     input.push(temp);
     
 });
 
-let total = 0;
+let cities = ['Faerun', 'Norrath', 'Tristram', 'AlphaCentauri', 'Arbre', 'Snowdin', 'Tambi', 'Straylight']
 
+let allCombinations = permutator(cities);
 
+let lowest = 99999;
 
-log(total);
+allCombinations.forEach(el => {
+    let totalDistance = 0;
+    if(el.length == 8){
+        for(let i = 1; i < el.length; i++){
+            let c1 = el[i - 1];
+            let c2 = el[i];
+            input.forEach(element => {
+                if((element.loc1 == c1 && element.loc2 == c2) || (element.loc2 == c1 && element.loc1 == c2)){
+                    totalDistance += element.distance;
+                }
+            });
+        }
+    }
+    if(parseInt(totalDistance) < lowest){
+        lowest = parseInt(totalDistance);
+    }
+});
+
+log(lowest);
+
+function permutator(inputArr) {
+    var results = [];
+  
+    function permute(arr, memo) {
+      var cur, memo = memo || [];
+  
+      for (var i = 0; i < arr.length; i++) {
+        cur = arr.splice(i, 1);
+        if (arr.length === 0) {
+          results.push(memo.concat(cur));
+        }
+        permute(arr.slice(), memo.concat(cur));
+        arr.splice(i, 0, cur[0]);
+      }
+  
+      return results;
+    }
+  
+    return permute(inputArr);
+  }
--- 2015/9b.js
+++ 2015/9b.js
@@ -4,7 +4,7 @@ const {log} = require('../common/log');
 
 let data = '';
 try {
-    data = fs.readFileSync('2015/inputs/10.txt', 'utf8');
+    data = fs.readFileSync('2015/inputs/9.txt', 'utf8');
 } catch (err) {
     console.error(err);
 }
@@ -12,13 +12,59 @@ try {
 let input = [];
 
 data.split('\r\n').forEach(element => {
-    let temp = element;
+    let arr = element.split(' ');
+    let temp = {
+        loc1: arr[0],
+        loc2: arr[2],
+        distance: parseInt(arr[4])
+    }
     input.push(temp);
     
 });
 
-let total = 0;
+let cities = ['Faerun', 'Norrath', 'Tristram', 'AlphaCentauri', 'Arbre', 'Snowdin', 'Tambi', 'Straylight']
 
+let allCombinations = permutator(cities);
 
+let highest = 0;
 
-log(total);
+allCombinations.forEach(el => {
+    let totalDistance = 0;
+    if(el.length == 8){
+        for(let i = 1; i < el.length; i++){
+            let c1 = el[i - 1];
+            let c2 = el[i];
+            input.forEach(element => {
+                if((element.loc1 == c1 && element.loc2 == c2) || (element.loc2 == c1 && element.loc1 == c2)){
+                    totalDistance += element.distance;
+                }
+            });
+        }
+    }
+    if(parseInt(totalDistance) > highest){
+        highest = parseInt(totalDistance);
+    }
+});
+
+log(highest);
+
+function permutator(inputArr) {
+    var results = [];
+  
+    function permute(arr, memo) {
+      var cur, memo = memo || [];
+  
+      for (var i = 0; i < arr.length; i++) {
+        cur = arr.splice(i, 1);
+        if (arr.length === 0) {
+          results.push(memo.concat(cur));
+        }
+        permute(arr.slice(), memo.concat(cur));
+        arr.splice(i, 0, cur[0]);
+      }
+  
+      return results;
+    }
+  
+    return permute(inputArr);
+  }
